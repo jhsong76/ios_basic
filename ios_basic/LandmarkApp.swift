ee6b858fbad892a6aa715d514debfcc8d9c6d8b0
jhsong <wngml203@naver.com>
2023-03-12 16:07:42 +0900

    fix: Show SwiftUI Inspection 사용

---
+++ ios_basic/LandmarkApp.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkApp.swift
+//  ios_basic
+//
+//  Created by 송주희 on 2023/03/12.
+//
+
+import SwiftUI
+
+struct LandmarkApp: View {
+    var body: some View {
+        Text("Hello, World!")
+            .foregroundColor(Color.orange)
+    }
+}
+
+struct LandmarkApp_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkApp()
+    }
+}
